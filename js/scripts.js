5248f8f7c2c8ae13830ae9d03d6d9de2f957aa00
Halle Tempest, Karina Roush and Kristen McKee <halle@>
2017-06-21 15:37:23 -0700

    change card IDs to numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
 (function() {
-
-  function onclick("#dealAgain") {
-    
-  }
-
-
   var cards = document.querySelectorAll(".card.effect__click");
   for ( var i  = 0, len = cards.length; i < len; i++ ) {
     var card = cards[i];
